e380eeb4387a385bd3f7a7bec81de2f2f0000428
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-09-28 09:51:05 +0400

    tst

--- src/test/versions/GetempMail.java
+++ src/test/versions/GetempMail.java
@@ -10,13 +10,13 @@ import org.junit.jupiter.api.Test;
 // Get more info from https://rapidapi.com/Privatix/api/temp-mail/
 public class GetempMail {
 
-    String url = "https://www.disify.com/api/email/emil_2005@mail.ru";
+    String url = "https://www.disify.com/";
 
     @Test
         public void testHello() {
 //        RestAssured.enableLoggingOfRequestAndResponseDe(LogDetail.ALL).;RestAssured.
         Response response = RestAssured
-                .given().log().all()
+                .given().log().all() // here we log all request data in console
                 .get(url)
                 .andReturn();
         response.prettyPrint();
