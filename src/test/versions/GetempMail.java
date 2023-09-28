9fb4f02ca41f30f387da2ce3987f9e27678bea15
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-09-28 09:29:39 +0400

    tst

--- src/test/versions/GetempMail.java
+++ src/test/versions/GetempMail.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
-// source https://rapidapi.com/Privatix/api/temp-mail/
+// https://rapidapi.com/Privatix/api/temp-mail/
 public class GetempMail {
 
     String url = "https://www.disify.com/api/email/emil_2005@mail.ru";
