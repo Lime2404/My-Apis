e9386d78adbec5ef4600df1dccbfc3810534288b
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-10-19 13:41:57 +0400

    selenide

--- src/test/tests/SimpleApis/ReqresTest.java
+++ src/test/tests/SimpleApis/ReqresTest.java
@@ -20,8 +20,13 @@ public class ReqresTest {
                 .get(URL+"api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
+
+        //в класс UserData мы извлекаем этот список пользователей из массива data
         // перебираем список и выбираем методы по очередности
+        // ниже х - это счетчик экземпляров класса
+        // тут мы проходим по каждому пользователю и сравниванием аватар
         users.forEach(x-> assertTrue(x.getAvatar().contains(x.getId().toString())));
+        // проверили что id содержится в avatar
     }
 
 }
