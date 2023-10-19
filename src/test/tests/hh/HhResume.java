ae818216990aa24d9e8c0b5ee45a10171837230b
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-10-19 06:54:24 +0400

    api tests start

--- src/test/tests/hh/HhResume.java
+++ src/test/tests/hh/HhResume.java
@@ -3,11 +3,33 @@ package hh;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.codeborne.selenide.Selenide.$x;
 
 public class HhResume {
     private final SelenideElement gender = $x("//span[@data-qa='resume-personal-gender']");
+
+    public static String GENDER = "Пол";
+
     public HhResume(String URL) {
         Selenide.open(URL);
     }
+    public Map<String, Object> getAttributes(){
+        return new HashMap<>(){{
+            put(GENDER, getGender());
+        }};
+    }
+
+    public String getGender(){
+        return gender.getText().equals("Мужчина") ? "М" : "Ж";
+    }
+//    public String getGender(){
+//        String genderValue = gender.getText();
+//        if(genderValue == "Мужчина"){
+//            return "M";
+//        }
+//        return "Ж";
+//    }
 }
--- src/test/tests/hh/hhLocal.java
+++ src/test/tests/hh/hhLocal.java
@@ -4,18 +4,28 @@ import core.BaseSelenideTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class hhLocal extends BaseSelenideTest {
-    private final static String URL = "file:///C:/TEMP/Resume";
+    private final static String URL = "file:///C:/TEMP/Resume.html";
 //    private final static String URL = "https://krisha.kz/a/show/687462531";
 
     @Test
     public void checkAttributesHashMap(){
+        HhResume hhResume = new HhResume(URL);
+        Map<String, Object> expectedAttributes = new HashMap<>();
+        expectedAttributes.put(HhResume.GENDER, "М");
 
-        HhResumePage hhResumePage = new HhResumePage(URL);
+        Map<String, Object> actualAttributes = hhResume.getAttributes();
+
+        Assert.assertEquals(expectedAttributes, actualAttributes);
+//        HhResumePage hhResumePage = new HhResumePage(URL);
 //emil.issabekov@gmail.com
 
 //        Response response = given() //RestAssured.
