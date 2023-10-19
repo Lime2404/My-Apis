c92025c571d50b8edf94799528a1e5e537134230
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-10-19 08:22:16 +0400

    selenide

--- src/test/tests/hh/HhResume.java
+++ src/test/tests/hh/HhResume.java
@@ -10,29 +10,38 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class HhResume {
     private final SelenideElement gender = $x("//span[@data-qa='resume-personal-gender']");
+    private final SelenideElement age = $x("//span[@data-qa='resume-personal-age']/span");
 
     public static String GENDER = "Пол";
 
     public HhResume(String URL) {
         Selenide.open(URL);
     }
-    public Map<String, Object> getAttributes(){
-        return new HashMap<>(){{
-            put(GENDER, getGender());
-        }};
+//    public Map<String, Object> getAttributes(){
+//        return new HashMap<>(){{
+//            put(GENDER, getGender());
+//        }};
+//    }
+    //можно регулярным выражением \D+ или его эквивалентом [^0-9] выбрать все символы которые не являются числом
+// а далее в коде Заменить все найденные символы на пустоту
+    public int getAge(){
+    return Integer.parseInt(age.getText().replaceAll("\\D+", ""));
     }
 
-//    public String getGender(){
-//        return gender.getText().equals("Мужчина") ? "М" : "Ж";
-//    }
-    public String getGender() {
-        String genderValue = gender.getText();
-        if (genderValue.equals("Мужчина")) {
 
-            genderValue = "M";
-        }
-        return genderValue;
-        }
+    public String getGender(){
+        return gender.getText().equals("Мужчина") ? "М" : "Ж"; //тернарный оператор - если условие
+        // условие в equals верно, товернуть М в противном случае Ж
+    }
+//    public String getGender() {
+//        String genderValue = gender.getText();
+//        if (genderValue.equals("Мужчина")) {
+//
+//            genderValue = "M";
+//        }
+//        return genderValue;
+//        }
+
 
 
 
