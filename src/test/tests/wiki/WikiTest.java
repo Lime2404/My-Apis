f30247224967571499aa4e8efc2ef26dd169cc45
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-11-10 10:33:14 +0400

    Href

--- src/test/tests/wiki/WikiTest.java
+++ src/test/tests/wiki/WikiTest.java
@@ -3,8 +3,11 @@ package wiki;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import core.BaseSelenideTest;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,5 +43,25 @@ public class WikiTest extends BaseSelenideTest {
         //3  Используя лямбда выражение мы каждый элемент из hrefs x записываем в links
         // зачеркнутое stream означает что мы используем два раза перебор так как stream уже есть в for each
         hrefs.stream().forEach(x->links.add(x.getAttribute("href")));
+//        int i = 0;
+
+        // получая ссылки в каждом элементе, мы можем перебирать их значения
+//        1
+    // открытие всех ссылок с помощью stream api и ссылочного метода selenide open
+        links.forEach(Selenide :: open); //аналог links.forEach(x->Selenide.open(x)) но для одного действия
+        // это ту мач.  Сейчас начнется быстрый скрол
+//        int i = 0;
+//        2
+    // Сравниваем ссвылку из браузера с тем что нам приходит из коллекции
+        for(int i = 0; i<links.size(); i++){
+            String listurl = links.get(i); // получаем ссылку. с которой будем работать
+            Selenide.open(listurl); //открываем полученную ссылку
+            // теперь получаем ссылку из адресной строки браузера
+            String currenturl = WebDriverRunner.getWebDriver().getCurrentUrl();
+//            System.out.println(currenturl + " " + listurl);
+            Assert.assertEquals(currenturl, listurl);
+        }
+// 15:26 https://youtu.be/eSYSQikJCTY?list=PLS-CH047rQ3U8iIUIb9gm3cyf9kbOSasP
+
     }
 }
