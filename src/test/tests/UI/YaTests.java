8c92859e08fddf0282f87771e3a3b78f303e07ce
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-11-21 11:10:05 +0400

    yandex_xpath

--- src/test/tests/UI/YaTests.java
+++ src/test/tests/UI/YaTests.java
@@ -2,6 +2,7 @@ package UI;
 
 import com.codeborne.selenide.Selenide;
 import core.BaseSelenideTest;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -14,6 +15,16 @@ public class YaTests extends BaseSelenideTest {
         driver.get(URL);
         driver.findElement(By.xpath("//input[@placeholder='найдётся всё']")).sendKeys("Время в Москве");
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//"))
+        driver.findElement(By.xpath("//button[contains(.,'Найти')]")).click();
+//        String text = driver.findElement(By.xpath("//*[@id=\"search-result\"]/li[2]/article/div[2]/div")).getText();
+//        System.out.println(text);
+//        Assert.assertTrue(text.contains("Москва"));
+    }
+
+    @Test
+    public void FailedTest() {
+        driver.get(URL);
+        driver.findElement(By.xpath("//input[@placeholder='найдётся всё']")).sendKeys("Время в Москве");
+        driver.findElement(By.xpath("//button[contains(.,'123456']")).click();
     }
 }
