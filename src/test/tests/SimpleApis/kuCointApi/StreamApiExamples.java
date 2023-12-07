0b4215c76a436aa0bc0c3a921a0d50c61767e1c3
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2023-12-07 08:42:46 +0400

    StreamWork

--- src/test/tests/SimpleApis/kuCointApi/StreamApiExamples.java
+++ src/test/tests/SimpleApis/kuCointApi/StreamApiExamples.java
@@ -6,10 +6,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
 public class StreamApiExamples {
@@ -22,7 +22,8 @@ public class StreamApiExamples {
             // метод extract извлекает ответ сервера куда-то
             // в данном случае мы извлекаем все крипты в класс и создаем список объектов этого класса?
             .extract().jsonPath().getList("data.ticker", TickerData.class);
-    // stream - это упрощенная функция for each где есть анонимная функция, которая позволяет нам выполнить
+//    int a = 0;
+     // stream - это упрощенная функция for each где есть анонимная функция, которая позволяет нам выполнить
     // какое-то действие или же серию действий.
     // у stream всегда должен быть конечный метод и в этом случае -применение сортировки и сохранение
     // ниже конечный метод collect засунет всю отфильтрованную информацию в список usdTickers
@@ -35,7 +36,7 @@ public class StreamApiExamples {
 //        System.out.println(test);
 
         Assert.assertTrue(usdTickers.stream().allMatch(x->x.getSymbol().endsWith("USDT")));
-//        int a = 0;
+        int a = 0;
     }
 
     @Test
@@ -45,17 +46,38 @@ public class StreamApiExamples {
             @Override
             public int compare(TickerData o1, TickerData o2) { // метод compare присутствует у интерфейса
                 // TickerData o1, TickerData o2 - объекты. Ниже возвращаем от большего к м. чтобы следовать highToLow
-                return o2.getChangeRate().compareTo(o2.getChangeRate());
+                return o2.getChangeRate().compareTo(o1.getChangeRate());
             }
         }).collect(Collectors.toList());
         // метод limit показывает сколько элементов мы можем достать
         List<TickerData> top10 = highToLow.stream().limit(10).collect(Collectors.toList());
 //        int a = 0;
-        Assert.assertTrue(top10.get(9).getSymbol().contains("FLAME"));
+        Assert.assertTrue(top10.get(9).getSymbol().contains("USDT"));
         System.out.println(top10.get(9).getSymbol() + "вот");
-        Assertions.assertEquals(top10.get(9).getSymbol(), "FLAME-USDT");
+//        Assertions.assertEquals(top10.get(9).getSymbol(), "FLAME-USDT");
 
     }
 
+    @Test // получаем список криптовалют самых низких за сутки
+    public void sortLowToHigh(){
+        List<TickerData> lowToHigh = getTickers.stream().filter(x->x.getSymbol().endsWith("USDT"))
+                .sorted(new TickerComparatorLow()).limit(10).collect(Collectors.toList());
+        int a = 0;
+    }
+
+    // Собираем класс в map. Преобразуем название валюты в маленькие буквы
     @Test
+    public void map(){
+        Map<String, Float> usd = new HashMap<>(); // ниже берем один тип данных, что-то с ним делаем и преобразуем в другой тип данных
+//        List<String> lowerCases = getTickers.stream().map(x->x.getSymbol().toLowerCase()).collect(Collectors.toList());
+        // метод map достал названия криптовалют и превратил их lowercase
+        //ниже будем обращаться к нашей хэш карте и вызывать у неё метод put
+        // через лямбду мы полученную информацию засовывали в map и дальше с ней можно как-то работать
+//        getTickers.forEach(x->usd.put(x.getSymbol(), Float.parseFloat(x.getChangeRate())));
+        // второй метод  - это работать с классом. Создадим TickerShort
+
+        List<TickerShort> shortList = new ArrayList<>();
+        getTickers.forEach(x->shortList.add(new TickerShort(x.getSymbol(), Float.parseFloat(x.getChangeRate()))));
+        int a = 0;
+    }
 }
